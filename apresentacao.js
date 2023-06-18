fec0be39bc78208b5367bb807bbe72d96a2bb12e
Italo Paixão Gomes <italo.gomes@mblabs.com.br>
2023-06-17 23:35:33 -0300

    Commit 10 - Criando Arquivos

---
+++ apresentacao.js
@@ -0,0 +1,13 @@
+const formatarMoeda = require('./util.js');
+
+module.exports = function gerarFaturaStr(fatura, calc) {
+  // console.log(fatura)
+  let faturaStr = `Fatura ${fatura.cliente}\n`;
+
+  for (let apre of fatura.apresentacoes) {
+    faturaStr += `  ${calc.repo.getPeca(apre).nome}: ${formatarMoeda(calc.calcularTotalApresentacao(apre))} (${apre.audiencia} assentos)\n`;
+  }
+  faturaStr += `Valor total: ${formatarMoeda(calc.calcularTotalFatura(fatura.apresentacoes))}\n`;
+  faturaStr += `Créditos acumulados: ${calc.calcularTotalCreditos(fatura.apresentacoes)} \n`;
+  return faturaStr;
+}
